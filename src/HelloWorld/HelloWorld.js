4502e1bef4d45b326f25070f711e99dd338e0856
Jozef Petro <jozefpetro2097@gmail.com>
2018-09-07 19:07:46 +0200

    lint-staged test

--- src/HelloWorld/HelloWorld.js
+++ src/HelloWorld/HelloWorld.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 const HelloWorld = ({ fontSize }) => (
   <h1 style={{ fontSize }}>Hello world !</h1>
 )
+debugger
 
 HelloWorld.propTypes = {
   fontSize: PropTypes.number.isRequired
