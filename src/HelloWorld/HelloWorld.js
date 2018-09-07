4d9b539e79e0ebf7a72245e6004418907162b2a0
Jozef Petro <jozefpetro2097@gmail.com>
2018-09-07 18:51:24 +0200

    docz setup

--- src/HelloWorld/HelloWorld.js
+++ src/HelloWorld/HelloWorld.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const HelloWorld = () =>
-  <h1>Hello world !</h1>
+const HelloWorld = ({ fontSize }) =>
+  <h1 style={{ fontSize }}>Hello world !</h1>
+
+HelloWorld.propTypes = {
+  fontSize: PropTypes.number.isRequired
+}
 
 export default HelloWorld
