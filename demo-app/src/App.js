d31ebdb9015550e7fb494973f15d158195832b23
Jozef Petro <jozefpetro2097@gmail.com>
2018-09-11 09:45:29 +0200

    updated react-scripts

---
+++ demo-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { HelloWorld } from 'react-logical-ui'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HelloWorld fontSize={20} />
+      </div>
+    );
+  }
+}
+
+export default App;
